efa75f9366c892fa1955198a27112cf761b5d039
Darren Foong <darrenfoong@gmail.com>
2020-03-28 01:26:52 +0800

    Add license and author

--- feign-form-spring/src/main/java/feign/form/spring/PojoSerializationWriter.java
+++ feign-form-spring/src/main/java/feign/form/spring/PojoSerializationWriter.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package feign.form.spring;
 
 import static feign.form.ContentProcessor.CRLF;
@@ -14,6 +30,10 @@ import lombok.val;
 
 import java.io.IOException;
 
+/**
+ *
+ * @author Darren Foong
+ */
 public abstract class PojoSerializationWriter extends AbstractWriter {
   @Override
   public boolean isApplicable(Object object) {
